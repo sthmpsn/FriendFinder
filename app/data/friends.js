1b650f5dd2af5dccbf5b8c3297fe811806171a5e
sthmpsn <sthmpsn@gmail.com>
2019-03-10 17:54:57 -0500

    adjusted so friendsArr will clear stale data and pull fresh data from DB

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const keys = require("../../keys");
 const mysql = require("mysql");
 
-
-// Store all the User data from MySQL
+// Store all the friends as objects from MySQL
 var friendsArr = [];
 
+// ===============================================================================
+// MySql Database Connection
+// ===============================================================================
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -19,7 +21,11 @@ connection.connect(function(err) {
     console.log("db connected as id " + connection.threadId);
 });
 
-function getFriends(){
+// ==================================================================================
+// Gets the all friends from table "friends" and add to the "friendsArr" as an object
+// ==================================================================================
+var getFriends = function (){
+    friendsArr = [];   // Clear the friendsArr to ensure only "fresh" data from DB is loaded
     connection.query("SELECT * FROM friends", function(err, result){
         for (var i=0; i < result.length; i++){
             friendsArr.push(
@@ -41,14 +47,15 @@ function getFriends(){
                 }
             )
         }
+        console.log("Friends in our DB\n",friendsArr);   // will log out the friendsArr AFTER the query loads the data
     });
-    console.log(friendsArr);
 }
-
 
 getFriends();   // initialize the friendsArr with mySql items
 
 // Export the friends array containing the DB data to be used in the api Routes
+// Export the db connection to be used for the api Push route
+// Export the getFriends function
 module.exports.connection = connection;
 module.exports.friendsArr = friendsArr;
 module.exports.getFriends = getFriends;
